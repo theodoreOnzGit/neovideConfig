b0aa14a9647602d279cc16d862f1607877169686
Theodore Ong at lenovo thinkpad windows E14 <theodore_ong@berkeley.edu>
2025-06-18 13:54:31 +0800

    removed dependencies on ultisnips for windows

--- windows_works/lua/plugins.lua
+++ windows_works/lua/plugins.lua
@@ -3,98 +3,89 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
 
-    -- git gutter (may clash with ubuntu's existing git-gutter
+	-- git gutter (may clash with ubuntu's existing git-gutter
 	-- if on linux mint)
-    -- use 'vim-scripts/vim-gitgutter'
-    'preservim/nerdtree',
-    'morhetz/gruvbox',
-    --'SirVer/ultisnips',
-    'williamboman/mason.nvim',
-    'williamboman/mason-lspconfig.nvim',
-    'neovim/nvim-lspconfig',
-    {
-	    'VonHeikemen/lsp-zero.nvim',
-	    branch = 'v2.x',
-	    dependencies = {
-		    -- LSP Support
-		    {                                      -- Optional
-			    'williamboman/mason.nvim',
-			    run = function()
-				    pcall(vim.cmd, 'MasonUpdate')
-			    end,
-		    },
+	-- use 'vim-scripts/vim-gitgutter'
+	'preservim/nerdtree',
+	'morhetz/gruvbox',
+	--'SirVer/ultisnips',
+	'williamboman/mason.nvim',
+	'williamboman/mason-lspconfig.nvim',
+	'neovim/nvim-lspconfig',
+	{
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v2.x',
+		dependencies = {
+			-- LSP Support
+			{                                      -- Optional
+				'williamboman/mason.nvim',
+				run = function()
+					pcall(vim.cmd, 'MasonUpdate')
+				end,
+			},
 
-		    -- Autocompletion from snippets
-		    {'hrsh7th/nvim-cmp'},     -- Required
-		    {'hrsh7th/cmp-nvim-lsp'}, -- Required
-		    {'L3MON4D3/LuaSnip'},     -- Required
+			-- Autocompletion from snippets
+			{'hrsh7th/nvim-cmp'},     -- Required
+			{'hrsh7th/cmp-nvim-lsp'}, -- Required
+			{'L3MON4D3/LuaSnip'},     -- Required
 
-		    -- buffer, path and commandline completion
-		    {'hrsh7th/cmp-buffer'},     -- Required
-		    {'hrsh7th/cmp-path'}, -- Required
-		    {'hrsh7th/cmp-cmdline'}, -- Required
+			-- buffer, path and commandline completion
+			{'hrsh7th/cmp-buffer'},     -- Required
+			{'hrsh7th/cmp-path'}, -- Required
+			{'hrsh7th/cmp-cmdline'}, -- Required
 
-	    }
-    },
-    {
-	    --"hrsh7th/nvim-cmp",
-	    --dependencies = {
-		    --	"quangnguyen30192/cmp-nvim-ultisnips",
-		    --	config = function()
-			    --		-- optional call to setup (see customization section)
-			    --		require("cmp_nvim_ultisnips").setup{}
-			    --	end,
-			    --	-- If you want to enable filetype detection based on treesitter:
-			    --	dependencies = { "nvim-treesitter/nvim-treesitter" },
-			    --}
-		    },
+		}
+	},
+	{
+		"hrsh7th/nvim-cmp",
+	},
 
-		    -- telescope 
-		    {
-			    'nvim-telescope/telescope.nvim', tag = '0.1.3',
-			    -- or                            , branch = '0.1.x',
-			    dependencies = { {'nvim-lua/plenary.nvim'} }
-		    },
+	-- telescope 
+	{
+		'nvim-telescope/telescope.nvim', tag = '0.1.3',
+		-- or                            , branch = '0.1.x',
+		dependencies = { {'nvim-lua/plenary.nvim'} }
+	},
 
-		    -- harpoon 
-		    'ThePrimeagen/harpoon',
+	-- harpoon 
+	'ThePrimeagen/harpoon',
 
 
-		    -- airline 
-		    'vim-airline/vim-airline',
-		    'vim-airline/vim-airline-themes',
+	-- airline 
+	'vim-airline/vim-airline',
+	'vim-airline/vim-airline-themes',
 
-		    -- tagbar
-		    'preservim/tagbar',
+	-- tagbar
+	'preservim/tagbar',
 
-		    -- fugitive 
-		    'tpope/vim-fugitive',
+	-- fugitive 
+	'tpope/vim-fugitive',
 
-		    -- vim latex
-		    'vim-latex/vim-latex',
+	-- vim latex
+	'vim-latex/vim-latex',
 
-		    -- vim align (in the arch repos)
-		    'junegunn/vim-easy-align',
+	-- vim align (in the arch repos)
+	'junegunn/vim-easy-align',
 
-		    -- ultisnips and snippets
-		    'honza/vim-snippets',
-		    -- tree, like NERDTree but another one in lua
+	-- ultisnips and snippets
+	'honza/vim-snippets',
+	-- tree, like NERDTree but another one in lua
 	'nvim-tree/nvim-tree.lua',
 	'nvim-tree/nvim-web-devicons',
 
